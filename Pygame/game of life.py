a6b567aacf7046cdcaf2a63e51801297bcbec343
Michael Erdey <michaelerdey@gmail.com>
2014-10-09 09:17:46 -0700

    multiplayer mode

--- Pygame/game of life.py
+++ Pygame/game of life.py
@@ -1,12 +1,13 @@
 import pygame, sys
 from pygame.locals import *
+from button import Button
 
 pygame.init()
 
 #game settings
 FPS = 10
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 550
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 650
 BUTTON_BAR_HEIGHT = 50
 GRID_WIDTH = WINDOW_WIDTH
 GRID_HEIGHT = WINDOW_HEIGHT - BUTTON_BAR_HEIGHT
@@ -20,7 +21,11 @@ BUTTON_HEIGHT = 30
 #colors (R, G, B)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
+PLAYER1_COLOR = RED
+PLAYER2_COLOR = BLUE
 TEXT_COLOR = BLACK
 
 FPSCLOCK = pygame.time.Clock()
@@ -28,7 +33,7 @@ FPSCLOCK = pygame.time.Clock()
 DISPLAY_SURFACE = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Conway's Game of Life")
 
-
+mode = 'competitive'
 status = 'stopped'
 grid = []
 for i in range(GRID_ROWS):
@@ -44,15 +49,19 @@ def main():
     buttons.append(stop_button)
     clear_button = Button(450, GRID_HEIGHT + 25, 100, 100, "Clear", clear)
     buttons.append(clear_button)
-    
-    spawnRPentomino(25, 25)
+
+    if mode == 'creative':
+        spawnRPentomino(25, 25)
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 cell = determineCellClick(pos[0], pos[1])
                 if cell:
-                    cellToggle(cell[0], cell[1])
+                    player = 'player1'
+                    if event.button == 3 and mode == 'competitive':
+                        player = 'player2'
+                    cellToggle(cell[0], cell[1], player)
                 else:
                     for button in buttons:
                         if button.hitbox().collidepoint(pos):
@@ -76,9 +85,12 @@ def main():
         
 def drawTile(x, y):
     if grid[y][x]:
+        if grid[y][x] == 1:
+            color = PLAYER1_COLOR
+        else:
+            color = PLAYER2_COLOR
         top_left = [x * BLOCK_WIDTH, y  * BLOCK_HEIGHT]
         rect = (top_left[0], top_left[1], BLOCK_WIDTH, BLOCK_HEIGHT)
-        color = BLACK
         pygame.draw.rect(DISPLAY_SURFACE, color, rect)
 
 def drawGrid():
@@ -98,23 +110,34 @@ def updateGrid():
         
     for y in range(len(grid) - 2):
         for x in range(len(grid[y]) - 2):
-            change = updateTileStatus(x + 1, y + 1)
-            new_grid[change[1]][change[0]] = change[2]
+            tileUpdate = updateTileStatus(x + 1, y + 1)
+            new_grid[tileUpdate[1]][tileUpdate[0]] = tileUpdate[2]
             
     grid = new_grid
 
     
 def updateTileStatus(x, y):
     global grid
-    
+
+    player1_cells = 0
+    player2_cells = 0
     live_neighbors = 0
     adjacentCells = getAdjacentCells(x, y)
     for cell in adjacentCells:
         if cell == 1:
+            player1_cells += 1
+            live_neighbors += 1
+        if cell == 2:
+            player2_cells += 1
             live_neighbors += 1
 
-    if live_neighbors == 3 or (live_neighbors == 2 and grid[y][x] == 1):
-        return (x, y, 1)
+    if live_neighbors == 3 or (live_neighbors == 2 and grid[y][x]):
+        if player1_cells > player2_cells:
+            return (x, y, 1)
+        elif player2_cells > player1_cells:
+            return (x, y, 2)
+        else: #player1_cells == player2_cells
+            return (x, y, 0)
     else:
         return (x, y, 0)
 
@@ -155,13 +178,16 @@ def determineCellClick(mouseX, mouseY):
     else:
         return False
 
-def cellToggle(x, y):
+def cellToggle(x, y, player):
     #player clicked on cell, toggle on/off
     global grid
     if grid[y][x]:
         grid[y][x] = 0
     else:
-        grid[y][x] = 1
+        if player == 'player1':
+            grid[y][x] = 1
+        if player == 'player2':
+            grid[y][x] = 2
 
 def spawnRPentomino(x, y):
     global grid
