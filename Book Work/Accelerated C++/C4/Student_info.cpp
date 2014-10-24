e36bf37064babfe6c34ff0160e6c18552ae15ec4
Michael Erdey <michaelerdey@gmail.com>
2014-10-24 16:45:11 -0700

    Reorganizing

---
+++ Book Work/Accelerated C++/C4/Student_info.cpp
@@ -0,0 +1,31 @@
+#include "Student_info.h"
+
+using std::istream;
+using std::vector;
+
+bool compare(const Student_info& x, const Student_info& y){
+	return x.name > y.name;
+}
+
+istream& read(istream& is, Student_info& s) {
+  is >> s.name >> s.midterm >> s.final;
+
+  read_hw(is, s.homework);
+  return is;
+}
+
+istream& read_hw(istream& in, vector<double>& hw) {
+  if (in) {
+    //get rid of previous contents
+    hw.clear();
+
+    double x;
+    while (in >> x) {
+      hw.push_back(x);
+    }
+    
+    //clear the stream so that input will work for next student
+    in.clear();
+  }
+  return in;
+}
