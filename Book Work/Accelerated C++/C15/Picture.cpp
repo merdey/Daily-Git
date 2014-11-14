faf1aa48b82afadc75c8a045e70249595cc0156b
Michael Erdey <michaelerdey@gmail.com>
2014-11-13 22:48:58 -0800

    done with chapter 15

--- Book Work/Accelerated C++/C15/Picture.cpp
+++ Book Work/Accelerated C++/C15/Picture.cpp
@@ -10,6 +10,66 @@ Pic_base::wd_sz String_Pic::width() const {
   return n;
 }
 
+void String_Pic::display(ostream& os, ht_sz row, bool do_pad) const {
+  wd_sz start = 0;
+  
+  if (row < height()) {
+    os << data[row];
+    start = data[row].size();
+  }
+
+  if (do_pad) {
+    pad(os, start, width());
+  }
+}
+
+void Frame_Pic::display(ostream& os, ht_sz row, bool do_pad) const {
+  if (row >= height()) {
+    if (do_pad) {
+      pad(os, 0, width());
+    }
+  } else {
+    if (row == 0 || row == height() - 1) {
+      //top or bottom row
+      os << string(width(), '*');
+    }
+    else if (row == 1 || row == height - 2) {
+      //second from top/bottom
+      os << "*";
+      pad(os, 1, width() - 1);
+      os << "*";
+    }
+    else {
+      //interior row
+      os << "* ";
+      p->display(os, row - 2, true);
+      os << " *";
+    }
+  }
+}
+
+void VCat_Pic::display(ostream& os, ht_sz row, bool do_pad) const {
+  wd_sz w = 0;
+  if (row < top->height()) {
+    //in top subpicture
+    top->display(os, row, do_pad);
+    w = top->width();
+  }
+  else if (row < height()) {
+    //bottom subpicture
+    bottom->display(os, row - top->height(), do_pad);
+    w = bottom->width();
+  }
+  if (do_pad) {
+    pad(os, w, width());
+  }
+}
+
+void HCat_Pic::display(ostream& os, ht_sz row, bool do_pad) const {
+  left->display(os, row, do_pad || row < right->height());
+  right->display(os, row, do_pad);
+}
+
 Picture frame(const Picture& pic) {
   return new Frame_Pic(pic.p);
 }
