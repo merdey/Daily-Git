7bf353f0709a66b7bc8bceac68ea0461f10e7f4c
Michael Erdey <michaelerdey@gmail.com>
2015-01-01 19:06:28 -0800

    useful views

--- Django/django_test/article/views.py
+++ Django/django_test/article/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
-from django.template.loader import get_template
-from django.template import Context
 from django.shortcuts import render_to_response
+from article.models import Article
 
-def hello(request):
-	name = "Michael"
-	html = "<html><body> Hi %s.</body></html>" % name
-	return HttpResponse(html)
+def articles(request):
+	return render_to_response('articles.html', {'articles': Article.objects.all()})
 	
-def hello_template(request):
-	return render_to_response('hello.html', {'name': 'Michael'})
+def article(request, article_id=1):
+	return render_to_response('article.html', {'article': Article.objects.get(id=article_id)})
+	
--- Django/django_test/django_test/urls.py
+++ Django/django_test/django_test/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'django_test.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-	url(r'^hello/$', 'article.views.hello_template'),
+    url(r'^$', 'django_test.views.home'),
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^articles/', include('article.urls')),
 )
